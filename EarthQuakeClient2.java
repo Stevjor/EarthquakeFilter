06e0b50632c8858fa75d1c3a6565ee91048cb576
Steven <stevjor20@DESKTOP-3JLQ7CI.localdomain>
2022-04-14 19:58:20 -0500

    Tested code.

--- EarthQuakeClient2.java
+++ EarthQuakeClient2.java
@@ -44,7 +44,8 @@ public class EarthQuakeClient2 {
     public void quakesWithFilter() { 
         EarthQuakeParser parser = new EarthQuakeParser(); 
         //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
-        String source = "data/nov20quakedatasmall.atom";
+        //String source = "data/nov20quakedata.atom";
+        String source = "https://www.dukelearntoprogram.com//course4/data/nov20quakedata.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);         
         System.out.println("read data for "+list.size()+" quakes");
 
@@ -54,28 +55,34 @@ public class EarthQuakeClient2 {
             System.out.println(qe);
         } */
         
-        /*
-         
-        Filter filteredMagnitude = new MagnitudeFilter(4.0, 5.0);
-        Filter filteredDepth = new DepthFilter(-35000.0, -12000.0);
+        Filter filteredMagnitude = new MagnitudeFilter(3.5, 4.5);
+        Filter filteredDepth = new DepthFilter(-55000.0, -20000.0);
         ArrayList<QuakeEntry> quakesFiltered = filter2(list, filteredMagnitude, 
                                              filteredDepth);
                                              
         for (QuakeEntry qe : quakesFiltered) {
             System.out.println(qe);
-        }*/
+        }
         
-        Location loc = new Location(35.42, 139.43);
-        double distance = 10000000.0;
+        System.out.println("There are " + quakesFiltered.size() + 
+        " such earthquakes found.");
+        
+        /*
+        Location loc = new Location(39.7392, -104.9903);
+        double distance = 1000000.0;
         
         Filter filteredDistance = new DistanceFilter(loc, distance);
-        Filter filteredPhrase = new PhraseFilter("end", "Japan");
+        Filter filteredPhrase = new PhraseFilter("end", "a");
         ArrayList<QuakeEntry> quakesFiltered = filter2(list, filteredDistance, 
                                              filteredPhrase);
                                              
         for (QuakeEntry qe : quakesFiltered) {
             System.out.println(qe);
         }
+        
+        System.out.println("There are " + quakesFiltered.size() + 
+        " such earthquakes found.");
+        */
     }
 
     /**
@@ -87,7 +94,8 @@ public class EarthQuakeClient2 {
     public void testMatchAllFilter() {
         EarthQuakeParser parser = new EarthQuakeParser();
         String fileName = "nov20quakedatasmall.atom";
-        String source = "data/" + fileName;
+        //String source = "data/" + fileName;
+        String source = "https://www.dukelearntoprogram.com//course4/data/nov20quakedata.atom";
         ArrayList<QuakeEntry> list = parser.read(source);
         
         /*
@@ -103,13 +111,13 @@ public class EarthQuakeClient2 {
         
         MatchAllFilter maf = new MatchAllFilter();
         
-        MagnitudeFilter filteredMagnitude = new MagnitudeFilter(0.0, 2.0);
+        MagnitudeFilter filteredMagnitude = new MagnitudeFilter(1.0, 4.0);
         filteredMagnitude.setName("Magnitude");
         
-        DepthFilter filteredDepth = new DepthFilter(-100000.0, -10000.0);
+        DepthFilter filteredDepth = new DepthFilter(-180000.0, -30000.0);
         filteredDepth.setName("Depth");
         
-        PhraseFilter filteredPhrase = new PhraseFilter("any", "a");
+        PhraseFilter filteredPhrase = new PhraseFilter("any", "o");
         filteredPhrase.setName("Phrase");
         
         
@@ -123,6 +131,9 @@ public class EarthQuakeClient2 {
             System.out.println(qe);
         }
         
+        System.out.println("There are " + appliedFilters.size() + 
+        " such earthquakes found.");
+        
         String names = maf.getName();
         System.out.println("Filters used are: " + names + ".");
     }
@@ -130,22 +141,23 @@ public class EarthQuakeClient2 {
     public void testMatchAllFilter2() {
         EarthQuakeParser parser = new EarthQuakeParser();
         String fileName = "nov20quakedatasmall.atom";
-        String source = "data/" + fileName;
+        //String source = "data/" + fileName;
+        String source = "https://www.dukelearntoprogram.com//course4/data/nov20quakedata.atom";
         ArrayList<QuakeEntry> list = parser.read(source);
         System.out.println("There are " + list.size() + 
         " earthquakes from the file " + fileName + ".");
         
         MatchAllFilter maf = new MatchAllFilter();
         
-        Location loc = new Location(36.1314, -95.9372);
+        Location loc = new Location(55.7308, 9.1153);
         
-        MagnitudeFilter filteredMagnitude = new MagnitudeFilter(0.0, 3.0);
+        MagnitudeFilter filteredMagnitude = new MagnitudeFilter(0.0, 5.0);
         filteredMagnitude.setName("Magnitude");
         
-        DistanceFilter filteredDistance = new DistanceFilter(loc, 10000000);
+        DistanceFilter filteredDistance = new DistanceFilter(loc, 3000000);
         filteredDistance.setName("Distance");
         
-        PhraseFilter filteredPhrase = new PhraseFilter("any", "Ca");
+        PhraseFilter filteredPhrase = new PhraseFilter("any", "e");
         filteredPhrase.setName("Phrase");
         
         maf.addFilter(filteredMagnitude);
@@ -157,6 +169,9 @@ public class EarthQuakeClient2 {
         for (QuakeEntry qe : appliedFilters) {
             System.out.println(qe);
         }
+        
+        System.out.println("There are " + appliedFilters.size() + 
+        " such earthquakes found.");
         
         String names = maf.getName();
         System.out.println("Filters used are: " + names + ".");
